bb4217b17d4220f0cad8efac0ab36cc4df9d9cb0
andrellgrillo <andrell.grillo@gmail.com>
2021-05-27 17:56:17 -0300

    finalizando com o SSG

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,12 @@
+
+import styles from './styles.module.scss'
+
+interface SubscribebuttonProps {
+   priceId: string
+}
+
+export function SubscribeButton({priceId}: SubscribebuttonProps) {
+   return(
+      <button type="button" className={styles.subscribeButton}>Subscribe Now</button>
+   )
+}
